deb89d79b6d9f5e5891565c70c52ac6fde1b1720
Kevin Roberts <kevin.roberts10@gmail.com>
2013-12-02 22:46:29 -0600

    new files

---
+++ src/main/java/CharacterMaker/domain/character/attributes/Magic.java
@@ -0,0 +1,17 @@
+package CharacterMaker.domain.character.attributes;
+
+import CharacterMaker.domain.character.Attribute;
+
+/**
+ * CharacterMaker.domain.character.attributes
+ *
+ * @author Kevin Roberts
+ *         Date: 12/02/2013
+ */
+
+public class Magic extends Attribute {
+
+    public Magic(String name, String description, int battleLevel) {
+        super(name, description, battleLevel);
+    }
+}
